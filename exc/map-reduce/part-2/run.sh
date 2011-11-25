e8812c79e836332647c3bcd346b3de82088b9cac
Chris Brown <xoebus@xoeb.us>
2011-11-25 19:31:49 +0000

    -jobconf is appreciated.

--- exc/map-reduce/part-2/run.sh
+++ exc/map-reduce/part-2/run.sh
@@ -1,10 +1,11 @@
 #/usr/bin/env sh
 
 hadoop jar /opt/hadoop/hadoop-0.20.2/contrib/streaming/hadoop-0.20.2-streaming.jar \
+              -jobconf mapred.job.name="s0824586 - Part 2" \
               -input /user/s0824586/coursework/input-train \
               -output /user/s0824586/coursework/output-part2 \
               -mapper mapper.rb \
               -file mapper.rb \
               -reducer reducer.rb \
               -file reducer.rb \
-              -D mapred.job.name="s0824586 - Part 2"
+              
