2b67d91ed59e73bb7c4cc428ba39fe95c99670cf
Chris Brown <xoebus@xoeb.us>
2011-11-25 21:45:09 +0000

    add todo.

--- exc/map-reduce/part-3/reducer.rb
+++ exc/map-reduce/part-3/reducer.rb
@@ -6,6 +6,7 @@ STDOUT.sync = true
 # To stop the word's features being repeated we only allow a word to print 5
 # features before refusing to print any more.
 
+# TODO: Make this use a hash to be more memory efficient
 used_lines = []
 
 ARGF.each do |line|  
