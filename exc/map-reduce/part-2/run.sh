bc46089129ded533ab5f9aff403d2c57a21aca26
Chris Brown <xoebus@xoeb.us>
2011-11-27 21:05:26 +0000

    Remove the existing output directory before running the job.

--- exc/map-reduce/part-2/run.sh
+++ exc/map-reduce/part-2/run.sh
@@ -1,5 +1,7 @@
 #/usr/bin/env sh
 
+hadoop fs -rmr /user/s0824586/coursework/output-part2
+
 hadoop jar /opt/hadoop/hadoop-0.20.2/contrib/streaming/hadoop-0.20.2-streaming.jar \
               -jobconf mapred.job.name="s0824586 - Part 2" \
               -input /user/s0824586/coursework/input-train \
