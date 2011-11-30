45ab1d0bd05963f329451c0970892339e74b4954
Chris Brown <cb@tardis.ed.ac.uk>
2011-11-30 19:51:56 +0000

    run over small data.

--- exc/map-reduce/part-2/run.sh
+++ exc/map-reduce/part-2/run.sh
@@ -4,7 +4,7 @@ hadoop fs -rmr /user/s0824586/coursework/output-part2
 
 hadoop jar /opt/hadoop/hadoop-0.20.2/contrib/streaming/hadoop-0.20.2-streaming.jar \
               -jobconf mapred.job.name="s0824586 - Part 2" \
-              -input /user/s0824586/coursework/input-train \
+              -input /user/s0824586/coursework/input-train-small \
               -output /user/s0824586/coursework/output-part2 \
               -mapper mapper.rb \
               -file mapper.rb \
