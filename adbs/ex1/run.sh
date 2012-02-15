759b805012cffcf79020870c96574546a28b5329
Chris Brown <xoebus@xoeb.us>
2012-02-15 16:24:50 +0000

    Added a run script.
    Signed-off-by: Chris Brown <xoebus@xoeb.us>

--- adbs/ex1/run.sh
+++ adbs/ex1/run.sh
@@ -1,3 +1,2 @@
-ant cleanAll
 ant
-java -cp dist/attica.jar org.dejave.attica.server.Database attica.properties
+java -cp dist/attica.jar org.dejave.attica.server.Database attica.properties < share/query.sql
